e78934863233d005ffed8d68e377f37d2a18ca92
Alexander Bagnall <ab667712@ohio.edu>
2017-09-13 11:43:22 -0400

    Remove program arguments left over from previous version. Currently the programs can't take command line arguments.

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,21 @@
 module Main where
 
-import Control.Monad
-import System.Environment (getArgs)
-import qualified Z3.Monad      as Z3
+import           Control.Monad
+import           System.Environment (getArgs)
+import qualified Z3.Monad as Z3
 
-import Ast
-import Compile (compileProg)
-import EvalZ3  (checkSat, evalCompileM)
-import Interp  (interpProg)
-import Parser  (parseProg)
+import           Ast
+import           Compile (compileProg)
+import           EvalZ3  (checkSat, evalCompileM)
+import           Interp  (interpProg)
+import           Parser  (parseProg)
 
 main :: IO ()
 main = do
   args <- getArgs
   if length args < 1 then error "Error: need input file" else do
     let file = head args
-    let p_args = [read arg :: Integer | arg <- tail args]
+    -- let p_args = [read arg :: Integer | arg <- tail args]
 
     -- Read in source file
     src <- readFile file
@@ -27,10 +27,10 @@ main = do
         
         -- Verify
         let verify = compileProg p
-         -- default loop bound is 10
+         -- default loop bound = 10, no assertions
         sol <- evalCompileM (10, []) (checkSat verify)
         putStrLn sol
         
         -- Evaluate
-        let result = interpProg p_args p
+        let result = interpProg p
         putStrLn $ show result
--- src/Interp.hs
+++ src/Interp.hs
@@ -9,14 +9,14 @@ import qualified Symtab (get)
 import Eval (evalAExp, runEvalAExp)
 
 -- Interpret a program by evaluating its AExp
-interpProg :: [Integer] -> Prog -> Integer
-interpProg args p =
+interpProg :: Prog -> Integer
+interpProg p =
   -- Create initial state with function bindings.
   -- We are sure to use foldl here, so that [keys acc] always refers to
   -- all of the keys added to the ctx thus far. Since the functions are
   -- listed in the order in which they appear in the program, the
   -- function scoping rules are implemented by this.
-  let initState = foldl (\acc f -> add (fid_of f) (Right (f, keys acc)) acc)
-                    empty (funs_of p) in
-    runEvalAExp initState (evalAExp (eval_of p))
+  let s = foldl (\acc f -> add (fid_of f) (Right (f, keys acc)) acc)
+          empty (funs_of p) in
+    runEvalAExp s (evalAExp (eval_of p))
 
